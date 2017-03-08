54a8d2f71b9c23e42a9cef1a3acc1d25b58e672b
Vytautas <vytaut1as@gmail.com>
2017-03-08 16:05:02 +0200

    Prideti du failai data ir engine, nebaigiau nes justina isvaro

---
+++ CalcData.java
@@ -0,0 +1,17 @@
+
+public class CalcData implements Iinput {
+	
+	private String input;
+	private String []skstr;
+	private String action;
+	private double a;
+	private double b;
+
+	@Override
+	public void makeInputString(String strLine) {		
+		input = strLine;		
+	}
+	
+	
+
+}
