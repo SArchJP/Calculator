998a5057caaa5d5f522fc7120bbf5c41b3fa897e
petrulaitiene <dalia.petrulaitiene2@gmail.com>
2017-03-08 14:14:41 +0200

    Basic functionality interface created, for calculator functionality

---
+++ IBasicFunctionality.java
@@ -0,0 +1,11 @@
+
+public interface IBasicFunctionality {
+	
+	public double add(double a, double b);
+	public double substract(double a, double b);
+	public double multiply(double a, double b);
+	public double divide(double a, double b);
+	public double power(double a, double b);
+	
+	
+}
